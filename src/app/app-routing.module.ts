9f09e98f989a5a2f91e4a9ce7791b403a2493fec
olehnahorniak <oleh.nahorniak@gmail.com>
2021-12-04 21:03:11 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
-const routes: Routes = [];
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Index', pathMatch:'full'},
+  { path: 'Index', component: IndexComponent},
+  { path: 'component1', component: Component1Component},
+  { path: 'component2', component: Component2Component},
+
+  { path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { Component2Component } from './component2/component2.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    NotfoundComponent,
+    Component2Component,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
