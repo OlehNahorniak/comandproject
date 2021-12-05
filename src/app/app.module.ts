b2c0b7060d0888aad9a7439b3842cced172333ad
olehnahorniak <oleh.nahorniak@gmail.com>
2021-12-05 16:57:24 +0200

    sunday end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 import { Component1Component } from './component1/component1.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { Component2Component } from './component2/component2.component';
@@ -13,25 +14,26 @@ import { Component5Component } from './component5/component5.component';
 import { Component6Component } from './component6/component6.component';
 import { Component7Component } from './component7/component7.component';
 import { Component8Component } from './component8/component8.component';
-import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component,
-    NotfoundComponent,
-    Component2Component,
     IndexComponent,
+    Component1Component,    
+    Component2Component,    
     Component3Component,
     Component4Component,
     Component5Component,
     Component6Component,
     Component7Component,
     Component8Component,
-    MainComponent
+    NotfoundComponent,
+    MainComponent,  
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations:[
+    trigger('text' , [ 
+      state('start',style({
+        fontStyle: 'italic'
+    })),
+    state('end',style({
+      fontStyle: 'normal'
+    })),
+    transition('start => end', animate(1500)),
+    transition('end => start', animate(1500)),
+    ]) ,
+    trigger('color', [
+      state('white', style({
+        color: 'white'
+      })),
+      state('red', style({
+        color: 'red'
+      })),
+      transition('white => red', animate(1500)),
+    transition('red => white', animate(1500)),
+    ])
+  ]  
+  
 })
 export class MainComponent implements OnInit {
+
+  public textState ="start";
+  public colorState = "white";
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  changeText(){    
+    this.textState = 'end';
+    setTimeout(() => {
+      this.textState = 'start';
+    } , 3000);
+    }
+
+  hoverIn(){
+    this.textState = 'end';
+  }
+  hoverOut(){
+    this.textState = 'start';
+  }
+
+changeColor(){    
+    this.colorState = 'red';
+    setTimeout(() => {
+      this.colorState = 'white';
+    } , 3000);
+    }
 
 }
