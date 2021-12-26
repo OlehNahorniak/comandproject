bc92aded1300bd3f6991578fc11037814235d62d
olehnahorniak <oleh.nahorniak@gmail.com>
2021-12-26 14:10:43 +0200

    final

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { illuminat } from '../animation';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
+  animations: [
+    illuminat,
+    trigger('text', [
+      state('start', style({
+        fontStyle: 'italic'
+      })),
+      state('end', style({
+        fontStyle: 'normal'
+      })),
+      transition('start => end', animate(1500)),
+      transition('end => start', animate(1500)),
+    ]),
+    trigger('color', [
+      state('white', style({
+        color: 'white'
+      })),
+      state('purple', style({
+        color: 'purple'
+      })),
+      transition('white => purple', animate(1500)),
+      transition('purple => white', animate(1500)),
+    ])
+  ]
 })
 export class IndexComponent implements OnInit {
-
+  illuminationState: 'normal' | 'illuminat' = 'normal';
+  textState = "start";
+  colorState = "white";
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+  changeText() {
+    this.textState = 'end';
+    setTimeout(() => {
+      this.textState = 'start';
+    }, 3000);
+  }
+
+  hoverIn() {
+    this.textState = 'end';
+  }
+  hoverOut() {
+    this.textState = 'start';
+  }
+  start() {
+    this.illuminationState = 'illuminat';
+  }
+  end() {
+    this.illuminationState = 'normal';
+  }
+
+  changeColor() {
+    this.colorState = 'purple';
+    setTimeout(() => {
+      this.colorState = 'white';
+    }, 3000);
   }
 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,66 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { illuminat } from '../animation';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  animations: [
-    illuminat,
-    trigger('text', [
-      state('start', style({
-        fontStyle: 'italic'
-      })),
-      state('end', style({
-        fontStyle: 'normal'
-      })),
-      transition('start => end', animate(1500)),
-      transition('end => start', animate(1500)),
-    ]),
-    trigger('color', [
-      state('white', style({
-        color: 'white'
-      })),
-      state('red', style({
-        color: 'red'
-      })),
-      transition('white => red', animate(1500)),
-      transition('red => white', animate(1500)),
-    ])
-  ]
+  animations: [illuminat]
 
 })
 export class MainComponent implements OnInit {
-  illuminationState: 'normal' | 'illuminat' = 'normal';
-  textState = "start";
-  colorState = "white";
+  illuminationState: 'normal' | 'illuminat' = 'normal';  
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  changeText() {
-    this.textState = 'end';
-    setTimeout(() => {
-      this.textState = 'start';
-    }, 3000);
-  }
-
-  hoverIn() {
-    this.textState = 'end';
-  }
-  hoverOut() {
-    this.textState = 'start';
-  }
-
-  changeColor() {
-    this.colorState = 'red';
-    setTimeout(() => {
-      this.colorState = 'white';
-    }, 3000);
-  }
+  } 
 
   start() {
     this.illuminationState = 'illuminat';
